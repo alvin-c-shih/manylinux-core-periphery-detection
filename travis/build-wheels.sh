b43bdf2cccc941440da8a0e214ef5eebddf8de48
Sadamori Kojaku <freesailing4046@gmail.com>
2018-09-05 12:42:32 +0100

    +

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,10 +3,10 @@ set -e -x
 
 # Install a system package required by our library
 yum install -y atlas-devel
-"${PYBIN}/pip" install pybind11 
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install pybind11 
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
